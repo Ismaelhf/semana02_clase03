c47ed85f0cb6801e22b36b89682d32f6219d2b17
Ismael Huamani Franco <ismaelhf.88@gmail.com>
2020-10-14 20:50:20 -0500

    commit flecha

--- ejercicios.js
+++ ejercicios.js
@@ -17,3 +17,13 @@ const addArray = (array, array2) =>{
 }
 
 addArray(['ismael','fredy'], 'carlos');
+
+// 4) migrar la función bill a arrow function
+const bill = (products,tax) =>{
+    let total = 0;
+    for (let i = 0; i < products.length; i++) {
+        total += products[i] + products[i] * tax;
+    }
+    return total;
+}
+console.log('bill', bill([10,12,13], 0.18));
